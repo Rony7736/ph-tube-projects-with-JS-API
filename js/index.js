8f5ee87e230848a3e900fa01f991601bc1b63ac5
Rony7736 <ronychakrabarty99@gmail.com>
2024-10-05 01:48:36 +0600

    completed ph-tube-projects-with-JS-API

--- js/index.js
+++ js/index.js
@@ -31,10 +31,10 @@ const loadCategories = () => {
 }
 
 
-const loadVideos = () => {
+const loadVideos = (searchText = "") => {
     
     // fetch the data 
-    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
     .then(res => res.json())                
     .then(data => displayVideos(data.videos))
     .catch(error => console.log(error))
@@ -206,6 +206,9 @@ const displayCategories = (categories) => {
     })                        
 }
 
-
+document.getElementById('search-input').addEventListener('keyup', (event) => {
+    loadVideos(event.target.value);
+    
+})
 loadCategories()
 loadVideos()
---
+++ js/summary.js
@@ -0,0 +1,15 @@
+
+/**
+ * 1. load and display data initially
+ * 2. on click load and display data
+ * 3. Complex object accessing
+ * 4. toggle active class functionality
+ * 5. search functionality
+ * 6. load and display single data
+ * 7. display data in a modal
+ * 8. load and display single data
+ * 9. Conditional rendaring if array dont have any data
+ * 10. Conditional rendaring :: if a value empty
+ * 11. Conditional rendaring :: if a value length is zero
+ * 
+ */
